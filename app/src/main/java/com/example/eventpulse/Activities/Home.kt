6238021f80bbb7fbefd20aea13ced77ea7a9513b
VincentNdegwa <ndegwavincent7@gmail.com>
2023-10-24 11:18:59 +0300

    started the home page

--- app/src/main/java/com/example/eventpulse/Activities/Home.kt
+++ app/src/main/java/com/example/eventpulse/Activities/Home.kt
@@ -1,12 +1,28 @@
 package com.example.eventpulse.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eventpulse.Data.login.UserLogin
 import com.example.eventpulse.R
+import com.example.eventpulse.databinding.ActivityHomeBinding
+import com.google.gson.Gson
 
 class Home : AppCompatActivity() {
+    var gson = Gson()
+    var loading:Boolean = true
+    private lateinit var bind:ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        loading = true
+        var data = intent.getStringExtra("putExtra")
+        var userData = gson.toJson(data, UserLogin::class.java)
+//        if (loading == true){
+//            setContentView(R.layout.main_loading)
+//        }else{
+            bind = ActivityHomeBinding.inflate(layoutInflater)
+            setContentView(bind.root)
+//        }
+
     }
 }
--- app/src/main/java/com/example/eventpulse/Activities/Login.kt
+++ app/src/main/java/com/example/eventpulse/Activities/Login.kt
@@ -1,26 +1,48 @@
 package com.example.eventpulse.Activities
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Toast
+import com.example.eventpulse.Data.login.UserLogin
+import com.example.eventpulse.R
 import com.example.eventpulse.Request.DataRequest
 import com.example.eventpulse.databinding.ActivityLoginBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.util.Objects
 
 class Login : AppCompatActivity() {
     private lateinit var bind: ActivityLoginBinding
+    var gson= Gson()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        bind = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(bind.root)
-        bind.loginButton.setOnClickListener { this.navigateToDash() }
-        bind.createAcc.setOnClickListener {
-            this.navigateCreateAcc()
+        var sharedPref = getSharedPreferences("user", Context.MODE_PRIVATE).getString("credentials", null)
+        println(sharedPref)
+        if (sharedPref !== null){
+            setContentView(R.layout.main_loading)
+            var dataType = object : TypeToken<HashMap<String,String>>(){}.type
+            var dataHashMap = gson.fromJson<HashMap<String,String>>(sharedPref, dataType)
+            println(dataHashMap)
+            if (!dataHashMap["email"].isNullOrEmpty() && !dataHashMap["password"].isNullOrEmpty()){
+                this.navigateToDash(dataHashMap["email"].toString(), dataHashMap["password"].toString())
+            }
+        }else{
+            setContentView(bind.root)
+            var email = bind.emailInput.text.toString().trim()
+            var pass = bind.passwordInput.text.toString().trim()
+            bind.loginButton.setOnClickListener { this.navigateToDash(email,pass) }
+            bind.createAcc.setOnClickListener {
+                this.navigateCreateAcc()
+            }
         }
+
     }
 
-    private fun navigateToDash(){
-        var email = bind.emailInput.text.toString().trim()
-        var pass = bind.passwordInput.text.toString().trim()
+    private fun navigateToDash(email: String, pass:String){
         var params = HashMap<String, String>()
 
         params["email"] = email
@@ -31,14 +53,23 @@ class Login : AppCompatActivity() {
             params,
             "api/user/login",
             onSuccess = { data ->
-                println(data)
+               var userData = gson.fromJson(data,UserLogin::class.java)
+               if ( userData.error){
+                   Toast.makeText(this,userData.message, Toast.LENGTH_LONG).show()
+               }else{
+                   var dashIntent = Intent(this, Home::class.java)
+                   getSharedPreferences("user", Context.MODE_PRIVATE)
+                       .edit().putString("credentials", gson.toJson(params))
+                       .apply()
+                   dashIntent.putExtra("userData", data)
+                   startActivity(dashIntent)
+               }
                         },
             onError = { error ->
               println(error)
             } )
 
-//        var dashIntent = Intent(this, Home::class.java)
-//        startActivity(dashIntent)
+
     }
     private  fun navigateCreateAcc(){
         var createAccIntent = Intent(this, Register::class.java)
