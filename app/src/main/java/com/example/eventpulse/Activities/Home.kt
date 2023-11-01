38277e1fb8dd3da61bd2082d78535f7fc8908f4c
VincentNdegwa <ndegwavincent7@gmail.com>
2023-11-01 13:17:34 +0300

    worked on the fragment transaction

--- app/src/main/java/com/example/eventpulse/Activities/Home.kt
+++ app/src/main/java/com/example/eventpulse/Activities/Home.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.view.GravityCompat
@@ -21,6 +22,7 @@ import com.example.eventpulse.Data.homeData.DashData
 import com.example.eventpulse.Data.login.UserLogin
 import com.example.eventpulse.Data.search.searchData
 import com.example.eventpulse.Fragments.Events
+import com.example.eventpulse.Fragments.HomeEvents
 import com.example.eventpulse.R
 import com.example.eventpulse.Request.DataRequest
 import com.example.eventpulse.databinding.ActivityHomeBinding
@@ -37,22 +39,43 @@ class Home : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bind = ActivityHomeBinding.inflate(layoutInflater)
         loading = true
+        setContentView(bind.root)
         var data = getSharedPreferences("user", Context.MODE_PRIVATE).getString("userData",null)
         if (loading == true && data != null){
             userData =  gson.fromJson(data, UserLogin::class.java)
-            this.renderData(data)
             this.eventListeners()
+            this.renderData()
+            this.navigate()
         }else{
             setContentView(R.layout.activity_login)
             Toast.makeText(this,"An Error Occurred", Toast.LENGTH_SHORT).show()
         }
 
+
+    }
+
+    private fun navigate() {
+        var trans = this.supportFragmentManager.beginTransaction()
+        trans.replace(R.id.home_frame, HomeEvents())
+        trans.commit()
+    }
+
+    private fun renderData() {
+        var userProfile =   bind.topFragment.findViewById<ImageView>(R.id.user_profile_image)
+        if (!userData.data.profile.profile_image.isNullOrEmpty()){
+            Glide.with(this)
+                .load(Variables().Url+userData.data.profile.profile_image)
+                .into(userProfile)
+        }else{
+            Glide.with(this).load(Variables().ImageUrl).into(userProfile)
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         getSharedPreferences("user",Context.MODE_PRIVATE).edit().remove("userData").apply()
     }
+
     private fun eventListeners(){
         var menuButton = bind.topFragment.findViewById<ImageView>(R.id.hamburger_button)
         menuButton.setOnClickListener{
@@ -60,96 +83,4 @@ class Home : AppCompatActivity() {
             drawer.openDrawer(GravityCompat.START)
         }
     }
-    private fun renderData(data: String) {
-        var userData = gson.fromJson(data, UserLogin::class.java)
-        Toast.makeText(this, "You are logged in as ${userData.data.email}", Toast.LENGTH_SHORT).show()
-        setContentView(R.layout.main_loading)
-        var params = HashMap<String, String>()
-        params["user_id"]= userData.data.id.toString()
-        DataRequest(this).post(params, "api/retrieve", onSuccess = {data->
-            var resData = gson.fromJson(data, DashData::class.java)
-            if (!resData.error){
-                setContentView(bind.root)
-                var userProfile =   bind.topFragment.findViewById<ImageView>(R.id.user_profile_image)
-                if (!userData.data.profile.profile_image.isNullOrEmpty()){
-                    Glide.with(this)
-                        .load(Variables().Url+userData.data.profile.profile_image)
-                        .into(userProfile)
-                }else{
-                    Glide.with(this).load(Variables().ImageUrl).into(userProfile)
-                }
-                this.renderLatestEvents(resData)
-                this.renderRecommenenData(resData)
-                this.renderUpcoming(resData)
-                this.renderTrending(resData)
-                var searchInput = findViewById<EditText>(R.id.search_input)
-                searchInput.setOnEditorActionListener{v, actionId, event->
-                    when(actionId){
-                        EditorInfo.IME_ACTION_DONE,EditorInfo.IME_ACTION_SEARCH->{
-                            this.searchEvents(searchInput)
-                            true
-                        }
-                        else->false
-                    }
-                }
-
-            }else{
-                setContentView(R.layout.activity_login)
-                Toast.makeText(this,"An Error Occurred", Toast.LENGTH_SHORT).show()
-            }
-        }, onError = {err-> println(err) })
-    }
-    private fun renderLatestEvents(resData: DashData) {
-        var adapter = LatestEventsAdapter(resData.latestEvents, this)
-        var recyclerView = findViewById<RecyclerView>(R.id.latest_events_recycler)
-        recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
-        recyclerView.adapter = adapter
-    }
-    private fun renderRecommenenData(resData: DashData) {
-        val adapter = RecomendendEventsAdapter(this, resData.recomendedEvents)
-        val recyclerView = findViewById<RecyclerView>(R.id.recommended_events_recycler)
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        recyclerView.adapter = adapter
-
-    }
-    private fun renderUpcoming(resData: DashData){
-        if (resData.upCommingEvents.isEmpty()){
-            bind.invisibleHolder.visibility = View.VISIBLE
-        }else{
-            val adapter = UpComingEventsAdapter(this, resData.upCommingEvents)
-            val recyclerView = findViewById<RecyclerView>(R.id.upcoming_events_recycler)
-            recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-            recyclerView.adapter = adapter
-        }
-
-    }
-
-    private fun renderTrending(resData: DashData){
-        val adapter = TrendingEventsAdapter(this, resData.trendingEvents)
-        val recyclerView = findViewById<RecyclerView>(R.id.trending_events_recycler)
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        recyclerView.adapter = adapter
-    }
-
-    private fun searchEvents(searchInput: EditText) {
-        if (searchInput.text.trim().isNotEmpty()){
-            var search = searchInput.text.trim()
-            var params = HashMap<String,String>()
-            params["search"] = search.toString()
-            DataRequest(this).post(params,"api/events/search", onSuccess = {data->
-                println(data)
-                var resData = gson.fromJson(data,searchData::class.java)
-                if (!resData.error){
-                    var trans = this.supportFragmentManager.beginTransaction()
-                    trans.replace(R.id.home_frame, Events.instance(data))
-                    trans.addToBackStack(null)
-                    trans.commit()
-                }
-            }, onError = {
-                err -> println(err)
-            })
-        }
-
-    }
-
 }
--- app/src/main/java/com/example/eventpulse/Fragments/HomeEvents.kt
+++ app/src/main/java/com/example/eventpulse/Fragments/HomeEvents.kt
@@ -1,60 +1,152 @@
 package com.example.eventpulse.Fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.eventpulse.Adapter.LatestEventsAdapter
+import com.example.eventpulse.Adapter.RecomendendEventsAdapter
+import com.example.eventpulse.Adapter.TrendingEventsAdapter
+import com.example.eventpulse.Adapter.UpComingEventsAdapter
+import com.example.eventpulse.Data.homeData.DashData
+import com.example.eventpulse.Data.login.UserLogin
+import com.example.eventpulse.Data.search.searchData
+import com.example.eventpulse.Modules.Variables
 import com.example.eventpulse.R
+import com.example.eventpulse.Request.DataRequest
+import com.example.eventpulse.databinding.FragmentHomeEventsBinding
+import com.google.gson.Gson
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeEvents.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeEvents : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var bind: FragmentHomeEventsBinding
+    private lateinit var userData:UserLogin
+    var gson = Gson()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_events, container, false)
+        bind = FragmentHomeEventsBinding.inflate(layoutInflater)
+        var loading= true
+        var data =requireActivity().getSharedPreferences("user", Context.MODE_PRIVATE).getString("userData",null)
+        if (loading == true && data != null){
+            userData =  gson.fromJson(data, UserLogin::class.java)
+            this.renderData(data)
+//            this.eventListeners()
+        }else{
+            Toast.makeText(requireContext(),"An Error Occurred", Toast.LENGTH_SHORT).show()
+        }
+        return bind.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeEvents.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeEvents().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+
+    private fun renderData(data: String) {
+        var userData = gson.fromJson(data, UserLogin::class.java)
+        Toast.makeText(requireContext(), "You are logged in as ${userData.data.email}", Toast.LENGTH_SHORT).show()
+        var params = HashMap<String, String>()
+        params["user_id"]= userData.data.id.toString()
+        DataRequest(requireContext()).post(params, "api/retrieve", onSuccess = { data->
+            var resData = gson.fromJson(data, DashData::class.java)
+            if (!resData.error){
+                this.renderLatestEvents(resData)
+                this.renderRecommenenData(resData)
+                this.renderUpcoming(resData)
+                this.renderTrending(resData)
+                var searchInput = bind.searchInput
+                searchInput.setOnEditorActionListener{v, actionId, event->
+                    when(actionId){
+                        EditorInfo.IME_ACTION_DONE, EditorInfo.IME_ACTION_SEARCH->{
+                            this.searchEvents(searchInput)
+                            true
+                        }
+                        else->false
+                    }
                 }
+
+            }else{
+                Toast.makeText(requireContext(),"An Error Occurred", Toast.LENGTH_SHORT).show()
             }
+        }, onError = {err-> println(err) })
+    }
+    private fun renderLatestEvents(resData: DashData) {
+        var adapter = LatestEventsAdapter(resData.latestEvents, requireContext())
+        var recyclerView = bind.latestEventsRecycler
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL,false)
+        recyclerView.adapter = adapter
+    }
+    private fun renderRecommenenData(resData: DashData) {
+        val adapter = RecomendendEventsAdapter(requireContext(), resData.recomendedEvents)
+        val recyclerView = bind.recommendedEventsRecycler
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = adapter
+
+    }
+    private fun renderUpcoming(resData: DashData){
+        if (resData.upCommingEvents.isEmpty()){
+            bind.invisibleHolder.visibility = View.VISIBLE
+        }else{
+            val adapter = UpComingEventsAdapter(requireContext(), resData.upCommingEvents)
+            val recyclerView = bind.upcomingEventsRecycler
+            recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            recyclerView.adapter = adapter
+        }
+
+    }
+
+    private fun renderTrending(resData: DashData){
+        val adapter = TrendingEventsAdapter(requireContext(), resData.trendingEvents)
+        val recyclerView = bind.trendingEventsRecycler
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = adapter
+    }
+
+    private fun searchEvents(searchInput: EditText) {
+        if (searchInput.text.trim().isNotEmpty()){
+            var search = searchInput.text.trim()
+            var params = HashMap<String,String>()
+            params["search"] = search.toString()
+            DataRequest(requireContext()).post(params,"api/events/search", onSuccess = { data->
+                println(data)
+                var resData = gson.fromJson(data, searchData::class.java)
+                if (!resData.error){
+                    var trans = requireActivity().supportFragmentManager.beginTransaction()
+                    trans.replace(R.id.home_frame, Events.instance(data))
+                    trans.addToBackStack(null)
+                    trans.commit()
+                }
+            }, onError = {
+                    err -> println(err)
+            })
+        }
+
+    }
+    companion object {
+      fun instance(data:String): HomeEvents {
+          var app = HomeEvents()
+          var args = Bundle()
+          args.putString("data", data)
+          app.arguments = args
+          return app
+      }
     }
 }
