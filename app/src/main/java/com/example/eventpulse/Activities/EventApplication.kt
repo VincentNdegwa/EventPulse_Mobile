5e96fb1fc8a69f7130d33e2472ff333c0298ed51
VincentNdegwa <ndegwavincent7@gmail.com>
2023-11-06 12:43:25 +0300

    fixed some issue

--- app/src/main/java/com/example/eventpulse/Activities/EventApplication.kt
+++ app/src/main/java/com/example/eventpulse/Activities/EventApplication.kt
@@ -2,11 +2,38 @@ package com.example.eventpulse.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.eventpulse.R
+import com.example.eventpulse.databinding.ActivityEventApplicationBinding
 
 class EventApplication : AppCompatActivity() {
+    private lateinit var bind:ActivityEventApplicationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_event_application)
+        bind = ActivityEventApplicationBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        this.eventListeners()
+    }
+
+    private fun eventListeners() {
+        bind.backButton.setOnClickListener{
+            super.onBackPressed()
+        }
+        bind.applicationButton.setOnClickListener{
+            this.startApplication()
+        }
+    }
+
+    private fun startApplication() {
+        if (this.validate()){
+
+        }else{
+            Toast.makeText(this, "Please fill the required fields", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validate(): Boolean {
+        return true
     }
 }
